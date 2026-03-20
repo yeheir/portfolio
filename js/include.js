e29b32e57110a19403142b10e343d776dcc1940d
yeheir <vinnikes03@gmail.com>
2026-03-20 01:06:09 +0100

    updated the inlude function

--- js/include.js
+++ js/include.js
@@ -1,5 +1,9 @@
 document.querySelectorAll("[page-to-include]").forEach(el => {
   fetch(el.getAttribute("page-to-include"))
     .then(res => res.text())
-    .then(data => el.innerHTML = data);
+    .then(data => {
+      const temp = document.createElement("div");
+      temp.innerHTML = data;
+      el.replaceWith(...temp.childNodes);
+    });
 });
